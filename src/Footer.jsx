2559cd830ef1777ad1287aefd20e41b19fd94d41
Joshua <joshuaoberlin7@gmail.com>
2025-09-02 22:11:10 +0700

    [Fix] Fix Footer Link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <footer className="max-w-7xl mx-auto px-4 py-8 mt-16 border-t border-slate-200 text-center text-slate-500">
       <p>
-        &copy; {new Date().getFullYear()} Masak Apa. Dibuat dengan penuh rasa.
+        &copy; {new Date().getFullYear()} Masak Apa.{" "}
+        <a color="inherit" href="https://www.instagram.com/jo.gidion/">
+          @jo.gidion
+        </a>{" "}
+        All rights reserved.
       </p>
     </footer>
   );
